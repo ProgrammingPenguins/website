b51fdc531ef6794c521147b6d388f6950e0d1537
DerRoflmeister <joey.rosicka@comcast.net>
2014-04-25 07:55:38 -0500

    Create People_with_Taste.php

---
+++ People_with_Taste.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>People with Taste</title>
+    <meta name="Graph Making - General Manipulating Devices" />
+    <meta description= "Making a graph, the equation from the graph. Not the other way around"
+    <?php include($_SERVER["DOCUMENT_ROOT"] . "/head.php"); ?>
+  </head>
+  <body>
+    <header>
+      <?php include($_SERVER["DOCUMENT_ROOT"] . "/menu.php"); ?>
+      <h1><a href=". /">Graph Making</a></h1>
+      <h2>You know it, yet your really don't.</h2>
+    </header>
+    <article>
+      <h1>
+      
